837a3b7d554a5030753185c75012f1615a1ffa43
roglf <rogflc21@htl-kaindorf.at>
2025-07-29 11:45:56 +0200

    Initial Commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import Home from "./components/Home.tsx";
 
-function App(props) {
+function App() {
     return (
-        <div></div>
+        <div>
+            <Routes>
+                <Route path="/" element= <Home/> />
+            </Routes>
+        </div>
     );
 }
 
